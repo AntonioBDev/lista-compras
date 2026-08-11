45532d3dca95fd6162bdc198683fb0355c866bbd
AntonioBDev <basi.dev9@gmail.com>
2026-08-11 01:24:38 -0600

    config

--- include/templates/header.php
+++ include/templates/header.php
@@ -1,5 +1,17 @@
+<?php 
+    $zonaHoraria = new DateTimeZone('America/Hermosillo');
+    $fecha = new DateTime('now', $zonaHoraria);
+    $formateador = new IntlDateFormatter(
+        'es_ES',
+        IntlDateFormatter::NONE, 
+        IntlDateFormatter::NONE,
+        null, null, "EEEE, d MMMM"
+    );
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,18 +19,20 @@
     <link rel="stylesheet" href="/src/css/main.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
+
 <body>
     <header>
         <div class="barra contenedor">
             <section class="perfil">
-                <i class="fa-solid fa-circle-user"></i>
-                <div>
-                    <p>today, 1 may</p>
+                <div class="name-perfil">
+                    <i class="fa-solid fa-circle-user"></i>
+                    <p>"Hola, Usuario"</p>
                 </div>
+                <p><?php echo $formateador->format($fecha)?></p>
             </section>
             <nav>
-                <a href="#">Crear</a>
-                <a href="#">Configuracion</a>
+                <a href="#"><i class="fa-solid fa-circle-plus"></i></a>
+                <a href="#"><i class="fa-solid fa-gear"></i></a>
             </nav>
         </div>
         <h2>Dashboard</h2>
--- index.php
+++ index.php
@@ -1,10 +1,26 @@
 <?php
-    declare(strict_types=1);
-    require 'include/funciones.php';
 
-    incluirTemplate('header');
+declare(strict_types=1);
+require 'include/funciones.php';
+
+incluirTemplate('header');
 ?>
+<section>
+    <h3>Lista de compras</h3>
+    <div class="lista-compras">
+        <article class="item-compra">
+            <img src="img/manzana.png" alt="Manzana" class="item-img">
+            <span class="item-nombre">Manzanas (1 kg)</span>
 
-<?php 
-    incluirTemplate('footer');
+            <label class="checkbox-container">
+        <input type="checkbox" class="check-item">
+        <span class="checkmark"></span>
+        <span class="label-texto">Comprado</span>
+      </label>
+        </article>
+    </div>
+</section>
+
+<?php
+incluirTemplate('footer');
 ?>
